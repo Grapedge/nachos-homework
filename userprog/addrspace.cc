f2322dccbf332e1ece82f306b2e95d61014dd700
grapedge <grapedge@gmail.com>
2020-12-25 15:35:23 +0800

    upload

--- userprog/addrspace.cc
+++ userprog/addrspace.cc
@@ -93,12 +93,12 @@ AddrSpace::AddrSpace(OpenFile *executable)
     // 初始大小需要：代码大小 + 需要初始化的数据大小
     frames = divRoundUp(noffH.code.size + noffH.initData.size, PageSize);
 
-    unsigned int numFrames = max(MaxNumPhysPages, frames + 1);
+    unsigned int numFrames = max(NumPhysPages, frames);
 
     ASSERT(numFrames <= freeMap->NumClear()); // check we're not trying
-                                                                          // to run anything too big --
-                                                                          // at least until we have
-                                                                          // virtual memory
+                                              // to run anything too big --
+                                              // at least until we have
+                                              // virtual memory
 
     DEBUG('a', "Initializing address space, num pages %d, size %d\n",
           numPages, size);
