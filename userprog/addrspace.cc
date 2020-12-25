4604e7c342b92745ba022d0bef7dfe31497d8b3f
grapedge <grapedge@gmail.com>
2020-12-25 16:23:19 +0800

    upload

--- userprog/addrspace.cc
+++ userprog/addrspace.cc
@@ -261,7 +261,7 @@ void AddrSpace::ReplacePage(int badVAddr)
     pageTable[newPage].valid = TRUE;
     pageTable[newPage].dirty = FALSE;
     pageTable[newPage].readOnly = FALSE;
-    printf("位置：%d 物理：%d\n", newPage, pageTable[newPage].physicalPage);
+    printf("物理内存信息：%s\n", machine->mainMemory);
     // 读取数据到内存
     executable->ReadAt(&(machine->mainMemory[pageTable[newPage].physicalPage]), PageSize, newPage * PageSize);
     Print();
